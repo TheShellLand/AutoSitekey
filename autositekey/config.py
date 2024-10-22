7bdb2a44b9982978b897b16f56f9d46d1a090266
naisanzaa <naisanza@gmail.com>
2024-10-23 03:33:52 +0800

    fix circular import

--- autositekey/config.py
+++ autositekey/config.py
@@ -1,7 +1,5 @@
 from automon import logging
 
-from autositekey.hotkey import _ALL_CLASSES
-
 log = logging.getLogger(__name__)
 log.setLevel(level=logging.DEBUG)
 
@@ -15,9 +13,4 @@ class AutoSiteKeyConfig(object):
         return f'{self.__dict__}'
 
     def is_ready(self) -> bool:
-        if self.test():
-            return True
-        return False
-
-    def test(self):
-        return _ALL_CLASSES
+        return True

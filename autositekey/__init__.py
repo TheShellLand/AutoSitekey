21997bf63ac77324f2bdef0b17c0db5e7b3708d6
naisanzaa <naisanza@gmail.com>
2024-10-23 03:33:52 +0800

    fix circular import

--- autositekey/__init__.py
+++ autositekey/__init__.py
@@ -1,2 +1,3 @@
-from .client import AutoSiteKeyClient
+from .client import AutoSiteKeyClient, Hotkey, Click, Type
 from .config import AutoSiteKeyConfig
+from .hotkey import _ALL_CLASSES
--- autositekey/client.py
+++ autositekey/client.py
@@ -23,19 +23,85 @@ class AutoSiteKeyClient(object):
         return self.browser.get(url=url)
 
     def is_ready(self) -> bool:
-        if self.config.is_ready() and self.run():
-            return True
+        try:
+            if self.config.is_ready() and self.run():
+                return True
+        except Exception as error:
+            log.error(error)
         return False
 
     def run(self, headless: bool = True):
-        self.browser.config.webdriver_wrapper = ChromeWrapper()
+        try:
+            self.browser.config.webdriver_wrapper = ChromeWrapper()
 
-        useragent = SeleniumUserAgentBuilder().get_top()
-        self.browser.config.webdriver_wrapper.set_user_agent(
-            useragent
-        )
+            useragent = SeleniumUserAgentBuilder().get_top()
+            self.browser.config.webdriver_wrapper.set_user_agent(
+                useragent
+            )
 
-        if headless:
-            self.browser.config.webdriver_wrapper.in_headless()
+            if headless:
+                self.browser.config.webdriver_wrapper.in_headless()
 
-        return self.browser.run()
+            return self.browser.run()
+        except Exception as error:
+            raise Exception(error)
+
+
+class Click(object):
+    """Object holds what to click. Optional: 'when' conditions"""
+
+    def __init__(self, xpath: str):
+        self._xpath = [xpath]
+        self._when = []
+
+    def when(self, xpath: str):
+        """xpath to click in browser"""
+        self._when.append(xpath)
+        return self
+
+    def xpath(self, xpath: str):
+        """xpath match for condition to be True"""
+        self._xpath.append(xpath)
+        return self
+
+
+class Type(object):
+    """Object holds what to type. Optional: 'when' conditions"""
+
+    def __init__(self, text: str):
+        self._text = [text]
+        self._when = []
+
+    def text(self, text: str):
+        """text to type in browser"""
+        self._text.append(text)
+        return self
+
+    def when(self, xpath: str):
+        """xpath match for condition to be True"""
+        self._when.append(xpath)
+        return self
+
+
+class Hotkey(AutoSiteKeyClient):
+    """Hotkey class intended to be inherited when used for other sites"""
+
+    _actions: [Click or Type]
+    _URL: str
+    _TEST: dict
+
+    def __repr__(self):
+        return f'{self.hotkey_name}'
+
+    @property
+    def hotkey_name(self):
+        return f'{type(self).__name__}'
+
+    @property
+    def actions(self) -> [Click or Type]:
+        """list of Click or Type actions"""
+        return self._actions
+
+    @property
+    def url(self):
+        return self._URL

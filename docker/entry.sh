8a20d9cd543890000951810a5853c1d0dc856c95
naisanza <naisanza@gmail.com>
2025-02-05 07:27:30 -0600

    [docker] found where selenim starts the x server

--- docker/entry.sh
+++ docker/entry.sh
@@ -5,13 +5,18 @@
 cd $(dirname $0) && set -ex
 
 #bash /opt/bin/entry_point.sh | tee selenium.log &
+bash /opt/bin/start-xvfb.sh &
+
+sleep 3
 
 set +x
+
 while true; do
   if [ ! -f selenium.log ]; then break; fi
-
   grep 'Started Selenium Standalone' selenium.log && break
+  grep 'STARTED XVFB' selenium.log && break
 done
+
 echo 'http://localhost:4444'
 set -x
 
